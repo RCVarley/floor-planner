70a1a765e821ee875029e253122abee2bd6ee4a7
Ryan Varley <rcvarley@outlook.com>
2025-12-02 12:04:24 +0000

    useEditor now bare bones

--- src/composables/useEditor.ts
+++ src/composables/useEditor.ts
@@ -1,7 +1,7 @@
 import {inject} from "vue"
 import type {EditorState} from "@/components/floorPlanEditor/FloorPlanEditor.vue"
 
-export const useEditor = () => {
+export const useEditorState = () => {
   const editor = inject<EditorState>('editor')
   if (!editor) {
     throw new Error('could not find editor state')
